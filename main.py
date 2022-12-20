8640bfdaba3b4c00aa15e4fba853d7385a4e90f5
hecker <pythondelay@gmail.com>
2022-12-20 15:35:53 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+
+def main():
+
+    """
+    https://scikit-learn.org/stable/datasets/toy_dataset.html#iris-plants-dataset
+    Множество Ирисов Фишера
+    1. длина наружной доли околоцветника (sepal length)
+    2. ширина наружной доли околоцветника (sepal width)
+    3. длина внутренней доли околоцветника (petal length)
+    4. ширина внутренней доли околоцветника (petal width)
+    """
+
+    # Task 1: Use scikit-learn to load iris dataset.
+    iris = datasets.load_iris()
+
+    """
+    feature - характеризующая черта предмета
+    example - любой предмет
+    class - подвид или группа, на которые делится предмет
+    """
+    # Task 2: Print out the number of features & number of examples in the iris dataset
+    print('Number of features ' + str(len(iris.feature_names)))
+    print('Number of examples ' + str(len(iris.target)))
+
+    # Task 3: Print out iris dataset classes
+    print('Number of classes ' + str(len(iris.target_names)))
+
+    # Task 4: Display Sepal width and Sepal length features using matplotlib library.
+    plt.figure(figsize=(8, 6))
+    plt.grid()
+    sepal_length = iris.data[:, 0]
+    sepal_width = iris.data[:, 1]
+    classes = iris.target
+    # Обозначения цветов
+    formatter = plt.FuncFormatter(lambda i, *args: iris.target_names[int(i)])
+    plt.scatter(sepal_length, sepal_width, c=classes)
+    plt.colorbar(ticks=[0, 1, 2], format=formatter)
+    # Sepal Length
+    plt.xlabel(iris.feature_names[0])
+    # Sepal Width
+    plt.ylabel(iris.feature_names[1])
+    plt.title('Visualization of Dataset')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
+
